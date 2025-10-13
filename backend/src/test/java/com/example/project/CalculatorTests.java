725e6fa245ae0064a667297d962816201307407a
fatlontra <antonio.faula@gmail.com>
2025-10-13 15:28:45 -0400

    Adjusted test files for add & sub operations

--- backend/src/test/java/com/example/project/CalculatorTests.java
+++ backend/src/test/java/com/example/project/CalculatorTests.java
@@ -12,40 +12,23 @@ package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
 
-  @Test
-  @DisplayName("1 + 1 = 2")
-  void addsTwoNumbers() {
-    Calculator calculator = new Calculator();
-    assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
-  }
+  @ParameterizedTest(name = "{0} - {1} = {2}", quoteTextArguments = false)
+  @CsvSource(textBlock = """
+          0,    1,  -1
+          1,    2,  -1
+          49,  51,  -2
+          1,  100, -99
+      """)
 
-  @Test
-  @DisplayName("1 - 1 = 0")
-  void subSingleDigitNumbers() {
+  void sub(int first, int second, int expectedResult) {
     Calculator calculator = new Calculator();
-    assertEquals(0, calculator.sub(1, 1), "1 - 1 should equal 0");
-
-  }
-
-  @Test
-  @DisplayName("21 - 1 = 20")
-  void subDoubleDigitNumbers() {
-    Calculator calculator = new Calculator();
-    assertEquals(20, calculator.sub(21, 1), "21 - 1 should equal 20");
-  }
-
-  @Test
-  @DisplayName("-5 - -1 = -4")
-  void subSingNegativeNumbers() {
-    Calculator calculator = new Calculator();
-    assertEquals(-4, calculator.sub(-5, -1), "-5 - -1 should equal -4");
+    assertEquals(expectedResult, calculator.sub(first, second),
+        () -> first + " - " + second + " should equal " + expectedResult);
 
   }
 
@@ -56,9 +39,11 @@ class CalculatorTests {
       49,  51, 100
       1,  100, 101
       """)
+
   void add(int first, int second, int expectedResult) {
     Calculator calculator = new Calculator();
     assertEquals(expectedResult, calculator.add(first, second),
         () -> first + " + " + second + " should equal " + expectedResult);
   }
+
 }
