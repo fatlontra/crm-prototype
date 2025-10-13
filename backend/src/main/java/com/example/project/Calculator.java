94a7a0a2f001e8c901c721d5dbed02ed2d579e13
fatlontra <antonio.faula@gmail.com>
2025-10-13 15:01:31 -0400

    subtract function

--- backend/src/main/java/com/example/project/Calculator.java
+++ backend/src/main/java/com/example/project/Calculator.java
@@ -12,8 +12,11 @@ package com.example.project;
 
 public class Calculator {
 
-	public int add(int a, int b) {
-		return a + b;
-	}
+  public int add(int a, int b) {
+    return a + b;
+  }
 
+  public int sub(int a, int b) {
+    return a - b;
+  }
 }
--- backend/src/test/java/com/example/project/CalculatorTests.java
+++ backend/src/test/java/com/example/project/CalculatorTests.java
@@ -19,23 +19,46 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
 
-	@Test
-	@DisplayName("1 + 1 = 2")
-	void addsTwoNumbers() {
-		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
-	}
+  @Test
+  @DisplayName("1 + 1 = 2")
+  void addsTwoNumbers() {
+    Calculator calculator = new Calculator();
+    assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+  }
 
-	@ParameterizedTest(name = "{0} + {1} = {2}", quoteTextArguments = false)
-	@CsvSource(textBlock = """
-			0,    1,   1
-			1,    2,   3
-			49,  51, 100
-			1,  100, 101
-			""")
-	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
-		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
-	}
+  @Test
+  @DisplayName("1 - 1 = 0")
+  void subSingleDigitNumbers() {
+    Calculator calculator = new Calculator();
+    assertEquals(0, calculator.sub(1, 1), "1 - 1 should equal 0");
+
+  }
+
+  @Test
+  @DisplayName("21 - 1 = 20")
+  void subDoubleDigitNumbers() {
+    Calculator calculator = new Calculator();
+    assertEquals(20, calculator.sub(21, 1), "21 - 1 should equal 20");
+  }
+
+  @Test
+  @DisplayName("-5 - -1 = -4")
+  void subSingNegativeNumbers() {
+    Calculator calculator = new Calculator();
+    assertEquals(-4, calculator.sub(-5, -1), "-5 - -1 should equal -4");
+
+  }
+
+  @ParameterizedTest(name = "{0} + {1} = {2}", quoteTextArguments = false)
+  @CsvSource(textBlock = """
+      0,    1,   1
+      1,    2,   3
+      49,  51, 100
+      1,  100, 101
+      """)
+  void add(int first, int second, int expectedResult) {
+    Calculator calculator = new Calculator();
+    assertEquals(expectedResult, calculator.add(first, second),
+        () -> first + " + " + second + " should equal " + expectedResult);
+  }
 }
